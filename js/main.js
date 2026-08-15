edc150f77d7a14f8bc448bd31f2bcb42c5d10eef
Leonardo <leonardoftdev@gmail.com>
2026-08-15 11:55:54 -0300

    refactor: simplifica a lógica de navegação da galeria de imagens

--- js/main.js
+++ js/main.js
@@ -1,34 +1,17 @@
-let nextBtn = document.querySelector(".gallery__btn--next")
-let prevBtn = document.querySelector(".gallery__btn--prev")
-let listImgs = document.querySelectorAll(".gallery__item")
-let contador = 0
+const nextBtn = document.querySelector(".gallery__btn--next");
+const prevBtn = document.querySelector(".gallery__btn--prev");
+const listImgs = document.querySelectorAll(".gallery__item");
+const total = listImgs.length;
+let contador = 0;
 
-nextBtn.onclick = function() {
-   document.querySelector("figure.is-active").classList.remove("is-active")
+nextBtn.onclick = function () {
+  listImgs[contador].classList.remove("is-active");
+  contador = (contador + 1) % total;
+  listImgs[contador].classList.add("is-active");
+};
 
-   if (contador < 3) {
-      contador++;
-   } else {
-      contador = 0;
-   }
-
-   listImgs[contador].classList.add("is-active")
-
-}
-
-prevBtn.onclick = function() {
-   document.querySelector("figure.is-active").classList.remove("is-active")
-
-   if (contador > 0) {
-      contador--;
-   } else {
-      contador = 3
-   }
-
-   listImgs[contador].classList.add("is-active")
-}
-
-
-
-
-
+prevBtn.onclick = function () {
+  listImgs[contador].classList.remove("is-active");
+  contador = (contador - 1 + total) % total;
+  listImgs[contador].classList.add("is-active");
+};
